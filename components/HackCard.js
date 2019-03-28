89a67c9e6a4d64338b319dc662d5c5ea2d22af03
Mohd Danish Yusuf <mddanishyusuf@Serenity.local>
2019-03-29 02:23:17 +0530

    custom routing defined

--- components/HackCard.js
+++ components/HackCard.js
@@ -4,7 +4,7 @@ const HackCard = (props) => {
     const issue = props.single_issue
     return (
         <div>
-            <Markdown>{issue.body}</Markdown>
+            {issue.body}
         </div>
     )
 }
--- components/Layout.js
+++ components/Layout.js
@@ -8,6 +8,7 @@ const Layout = props => (
         <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta charSet="utf-8" />
+            <title>{props.title}</title>
         </Head>
         <Header />
         {props.children}
