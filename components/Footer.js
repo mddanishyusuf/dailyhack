ccc2060a3ffa0ac4c243a76798fe29bf4ac704fe
mddanishyusuf <mddanishyusuf@gmail.com>
2019-04-06 19:32:15 +0530

    footer responsive on mobile

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { Twitter, GitHub, Users, Info } from 'react-feather';
+import { Twitter, GitHub, Users, Info, HelpCircle } from 'react-feather';
 
 const Footer = function(props) {
     return (
@@ -12,6 +12,7 @@ const Footer = function(props) {
                 </div>
                 <div className="footer-nav right-navbar">
                     <ul>
+                        <li><a href="https://github.com/mddanishyusuf/dailyhack#-how-to-add-tricks" target="_blank"><HelpCircle color={'#4618B1'} size={18}/> <span>How to contribute</span></a></li>
                         <li><a href="https://github.com/mddanishyusuf/dailyhack/blob/master/README.md" target="_blank"><Info color={'#4618B1'} size={18}/> <span>About Us</span></a></li>
                         <li><Link href="/contributors"><a><Users color={'#4618B1'} size={18}/> <span>Contributors</span></a></Link></li>
                         <li><a href="https://github.com/mddanishyusuf/dailyhack" target="_blank"><GitHub color={'#4618B1'} size={18}/> <span>GitHub</span></a></li>
@@ -24,10 +25,33 @@ const Footer = function(props) {
                     .footer {
                         background-color: rgb(240, 237, 237);
                         margin-bottom: -15px;
-                        display: flex;
-                        -moz-box-pack: justify;
-                        justify-content: space-between;
                     }
+
+                    @media screen and (min-width: 700px) {
+                        .footer {
+                            display: flex;
+                            -moz-box-pack: justify;
+                            justify-content: space-between;
+                        }
+                        
+
+                        .footer-nav ul{
+                            float: right;
+                        }
+        
+                        .footer-nav ul li{
+                            padding: 10px;
+                            -moz-box-align: center;
+                            align-items: center;
+                            float: left;
+                            list-style: none;
+                        }
+                        
+                        .footer-nav {
+                            padding: 40px;
+                        }
+                    }
+
                     .footer-inner {
                         padding: 40px;
                     }
@@ -38,20 +62,9 @@ const Footer = function(props) {
                     .footer-inner a{
                         color: #4618B1;
                     }
-                    .footer-nav {
-                        padding: 40px;
-                    }
 
-                    .footer-nav ul{
-                        float: right;
-                    }
-    
-                    .footer-nav ul li{
-                        padding: 10px;
-                        -moz-box-align: center;
-                        align-items: center;
-                        float: left;
-                        list-style: none;
+                    .footer-nav ul {
+                        padding: 0;
                     }
     
                     .footer-nav ul li a{
@@ -60,7 +73,17 @@ const Footer = function(props) {
                         color: #707070;
                         font-size: 0.9rem;
                     }
-    
+                    
+                    
+                    .footer-nav {
+                        padding: 40px;
+                    }
+        
+                    .footer-nav ul li{
+                        padding: 10px;
+                        list-style: none;
+                        display: inline-block;
+                    }
     
                     .footer-nav ul li span {
                         padding: 0px 5px;
